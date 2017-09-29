a467aef00294413adaefd062b8fe3859682bf6bb
DmitryTryput <dima.triput@gmail.com>
2017-09-30 00:01:30 +0300

    Lesson 4. Task 4.

--- src/LineArray.java
+++ src/LineArray.java
@@ -5,29 +5,32 @@ public class LineArray {
 
     public static void main(String[] args) {
         Random random = new Random();
-        int[][] array = new int[5][4];
+        int[][] array = new int[random.nextInt(10)][];
+            for (int i = 0; i < array.length; i++) {
+                array[i] = new int [random.nextInt(10)];
+            }
             for (int i = 0; i < array.length; i++) {
                 for (int j = 0; j < array[i].length; j++) {
                     array[i][j] = random.nextInt(100);
                 }
             }
         System.out.println(Arrays.deepToString(array));
-        System.out.println(Arrays.toString(linearize(array)));
+        System.out.println(Arrays.toString(lineArray(array)));
     }
 
-    public static int[] linearize(int[][] array) {
+    public static int[] lineArray(int[][] array) {
         int counter = 0;
         int size = 0;
-            for (int i = 0; i < array.length; i++) {
-                size += array[i].length;
-            }
+        for (int[] anArray : array) {
+            size += anArray.length;
+        }
         int[] lineArray = new int[size];
-            for (int i = 0; i < array.length; i++) {
-                for (int j = 0; j < array[i].length; j++) {
-                    lineArray[counter] = array[i][j];
-                    counter++;
-                }
+        for (int[] outerArray : array) {
+            for (int innerArray : outerArray) {
+                lineArray[counter] = innerArray;
+                counter++;
             }
+        }
         return lineArray;
     }
 }
