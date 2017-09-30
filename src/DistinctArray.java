1da3841e5ff2a02c1a8053d29f446ab0f0f65106
DmitryTryput <dima.triput@gmail.com>
2017-09-30 10:37:41 +0300

    Lesson 4. Task 2.

--- src/DistinctArray.java
+++ src/DistinctArray.java
@@ -15,16 +15,29 @@ public class DistinctArray {
     }
 
     public static int[] distinctElements(int[] array) {
+        int[] clone = Arrays.copyOf(array, array.length);
         int counter = 0;
-        Arrays.sort(array);
-            for (int i = 1; i < array.length; i++) {
-                if (array[i] == array[i - 1]) {
-                    array[i - 1] = 0;
+        Arrays.sort(clone);
+            for (int i = 1; i < clone.length; i++) {
+                if (clone[i] == clone[i - 1]) {
+                    clone[i - 1] = 0;
                 } else {
                     counter++;
                 }
             }
-        Arrays.sort(array);
-        return Arrays.copyOfRange(array, array.length - (counter + 1), array.length);
+        Arrays.sort(clone);
+        int[] sortedArray =  Arrays.copyOfRange(clone, clone.length - (counter + 1), clone.length);
+        boolean[] isPutted = new boolean[sortedArray.length];
+        int[] distinctArray = new int[sortedArray.length];
+        int distinctArrayCounter = 0;
+        for (int element : array) {
+            for (int j = 0; j < sortedArray.length; j++) {
+                if (element == sortedArray[j] && !isPutted[j]) {
+                    distinctArray[distinctArrayCounter++] = element;
+                    isPutted[j] = true;
+                }
+            }
+        }
+       return distinctArray;
     }
 }
