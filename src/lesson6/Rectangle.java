4049680a1ea75d2215f87cd311d8452dce6421bf
DmitryTryput <dima.triput@gmail.com>
2017-09-30 19:39:42 +0300

    Rectangle

---
+++ src/lesson6/Rectangle.java
@@ -0,0 +1,33 @@
+package lesson6;
+
+public class Rectangle {
+
+    private Point leftTop;
+    private Point rightBottom;
+
+    public Rectangle(Point leftTop, Point rightBottom) {
+        this.leftTop = leftTop;
+        this.rightBottom = rightBottom;
+    }
+
+    public Rectangle(int leftX, int leftY, int rightX, int rightY) {
+        leftTop = new Point(leftX, leftY);
+        rightBottom = new Point(rightX, rightY);
+    }
+
+    public double diameter(){
+        return  leftTop.distance(rightBottom);
+    }
+
+    public int square(){
+        return xSize() * ySize();
+    }
+
+    private int xSize() {
+        return Math.abs(Math.max(leftTop.getX(), rightBottom.getX()) - Math.min(leftTop.getX(), rightBottom.getX()));
+    }
+
+    private int ySize() {
+        return Math.abs(Math.max(leftTop.getY(), rightBottom.getY()) - Math.min(leftTop.getY(), rightBottom.getY()));
+    }
+}
