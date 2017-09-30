dd67581cf00a582768d6027892ca8c6296e27c4f
DmitryTryput <dima.triput@gmail.com>
2017-09-30 18:16:50 +0300

    Lesson 5. Task 1.

--- src/PalindromeString.java
+++ src/PalindromeString.java
@@ -1,14 +1,14 @@
 public class PalindromeString {
 
     public static void main(String[] args) {
-        String word = "dabcdcbad";
+        String word = "dAbcdcbad";
         System.out.println(isPalindrome(word));
     }
 
     public static boolean isPalindrome(String word) {
-        word.trim().toLowerCase();
-        for (int i = 0; i < word.length() / 2; i++) {
-            if (word.charAt(i) != word.charAt(word.length() - (i + 1))){
+        String test = word.trim().toLowerCase();
+        for (int i = 0; i < test.length() / 2; i++) {
+            if (test.charAt(i) != test.charAt(test.length() - (i + 1))){
                 return false;
             }
         }
