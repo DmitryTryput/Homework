62d9ff4348b6f1b27976bf1055432094ca909cc7
DmitryTryput <dima.triput@gmail.com>
2017-09-30 18:15:08 +0300

    Lesson 5. Task 1.

--- src/PalindromeString.java
+++ src/PalindromeString.java
@@ -1,11 +1,12 @@
 public class PalindromeString {
 
     public static void main(String[] args) {
-        String word = "dabcddcbad";
+        String word = "dabcdcbad";
         System.out.println(isPalindrome(word));
     }
 
     public static boolean isPalindrome(String word) {
+        word.trim().toLowerCase();
         for (int i = 0; i < word.length() / 2; i++) {
             if (word.charAt(i) != word.charAt(word.length() - (i + 1))){
                 return false;
