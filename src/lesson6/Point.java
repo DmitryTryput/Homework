e31068ab1e3b532f9dfd52c9bc82b835ee6304f9
DmitryTryput <dima.triput@gmail.com>
2017-09-30 19:36:40 +0300

    Point

---
+++ src/lesson6/Point.java
@@ -0,0 +1,32 @@
+package lesson6;
+
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point p) {
+        return Math.abs(Math.sqrt(Math.pow((getX() - p.getX()), 2) + Math.pow((getY() - p.getY()), 2)));
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
