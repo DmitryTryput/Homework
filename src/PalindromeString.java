53dfb306dc9a689105f8e80666427bf256ad6be9
DmitryTryput <dima.triput@gmail.com>
2017-09-30 11:21:27 +0300

    Lesson 5. Task 1.

---
+++ src/PalindromeString.java
@@ -0,0 +1,16 @@
+public class PalindromeString {
+
+    public static void main(String[] args) {
+        String word = "dabcddcbad";
+        System.out.println(isPalindrome(word));
+    }
+
+    public static boolean isPalindrome(String word) {
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - (i + 1))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
