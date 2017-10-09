f069dbccc79650c4d10c9e5affb7881664590d6e
DmitryTryput <dima.triput@gmail.com>
2017-10-09 19:59:15 +0300

    Quadrate

--- src/figures/Quadrate.java
+++ src/figures/Quadrate.java
@@ -2,13 +2,20 @@ package figures;
 
 public class Quadrate extends Rectangle {
 
-    public Quadrate(Point leftButtom, int sideLength) {
-        super(leftButtom, new Point(leftButtom.getX() + sideLength, leftButtom.getY() + sideLength));
+    private int sideLength;
+
+    public Quadrate(Point leftBottom, int sideLength) {
+        super(leftBottom, new Point(leftBottom.getX() + sideLength, leftBottom.getY() + sideLength));
+        this.sideLength = sideLength;
     }
 
     @Override
     public String toString() {
         return "Квадрат с кординатами левой нижней точки (" + getLeftBottom().getX() + "," +getLeftBottom().getY() +
                 ") и правой верхней точки (" + getRightTop().getX() + "," + getRightTop().getY() + ")";
+    }
+
+    public int getSideLength() {
+        return sideLength;
     }
 }
