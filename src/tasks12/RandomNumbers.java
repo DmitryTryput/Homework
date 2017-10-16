83aeb703e3931adee5a67160a9a64176442176fb
DmitryTryput <dima.triput@gmail.com>
2017-10-16 22:45:29 +0300

    RandomNumbers

--- src/tasks12/RandomNumbers.java
+++ src/tasks12/RandomNumbers.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class RandomNumbers {
 
     public static void main(String[] args) {
-        File file = createFile("numbers.txt");
+        File file = createFile("numbers" + File.separator + "txt");
         generateNumbers(file,1000);
         sortInFile(file);
     }
